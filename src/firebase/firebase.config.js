3d88230c60180bbdb725cae6a35689853b4e0ccb
Your Name~ <f.zihad333@gmail.com>
2022-11-08 11:31:01 +0600

    firebase added and deployed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADZPoZWXYOPesDcQ8VS-kC5K8nLD-q7_I",
+  authDomain: "tiffinbhai-83114.firebaseapp.com",
+  projectId: "tiffinbhai-83114",
+  storageBucket: "tiffinbhai-83114.appspot.com",
+  messagingSenderId: "120942467531",
+  appId: "1:120942467531:web:4fb46df74b721b3233fe4e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
