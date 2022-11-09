289a4d3785d1b6e4daf8f1ce06e770b480b0a279
Your Name~ <f.zihad333@gmail.com>
2022-11-09 22:58:22 +0600

    react photo view integration completed

--- src/shared/ServiceSingle/ServiceSingle.js
+++ src/shared/ServiceSingle/ServiceSingle.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const ServiceSingle = ({ service }) => {
-  const { name, img, description } = service;
+  const { name, img, description, pricing } = service;
 
   return (
     <div className="service-single-box card ">
-      <img src={img} className="card-img-top" alt="" />
-      <div className="card-body d-flex flex-column justify-content-between">
+      <PhotoProvider>
+        <PhotoView src={img}>
+          <img
+            src={img}
+            className="card-img-top cursor-pointer"
+            role="button"
+            alt=""
+          />
+        </PhotoView>
+      </PhotoProvider>
+      <div className="card-body d-flex flex-column gap-3 justify-content-between">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description.slice(0, 100)} ...</p>
+
+        <p className="card-text fw-semibold">
+          Service-Charge: <span className="text-danger">{pricing} </span> Taka
+        </p>
+
         <Link to="#" className="btn btn-outline-warning text-dark fw-semibold">
           See Details
         </Link>
